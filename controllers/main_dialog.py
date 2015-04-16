93c1a85c571408d3b72dda10640e83239068ef3c
Vitaliy Zholobitskiy <just1ce2194@gmail.com>
2015-04-17 02:26:07 +0300

    Added ui for GetTopNGroupsByLocation

--- controllers/main_dialog.py
+++ controllers/main_dialog.py
@@ -3,19 +3,20 @@ from PyQt5.QtCore import QDateTime
 from PyQt5.uic import loadUi
 
 from controllers.send_groups import SendGroupsDialog
+from controllers.select_groups import SelectGroupsDialog
 
 
 class MainDialog(QWidget):
   def __init__(self):
     QWidget.__init__(self)
     self.ui = loadUi('uis/main_dialog.ui', self)
-    self.ui.btnTest.clicked.connect(self.__on_test_btn_clicked)
+    self.ui.btnSelectGroups.clicked.connect(self.__on_select_groups_btn_clicked)
     self.ui.btnSengGroups.clicked.connect(self.__on_seng_groups_btn_clicked)
 
-  def __on_test_btn_clicked(self):
-    self.ui.lblMessage.setText('Hello There!' + '\n'+ QDateTime.currentDateTime().toString())
+  def __on_select_groups_btn_clicked(self):
+    dialog = SelectGroupsDialog()
+    dialog.show()
 
   def __on_seng_groups_btn_clicked(self):
-    self.ui.lblMessage.setText('Hello World!')
     dialog = SendGroupsDialog()
     dialog.show()
---
+++ controllers/select_groups.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtCore import QDateTime
+from PyQt5.uic import loadUi
+
+class SelectGroupsDialog(QWidget):
+  def __init__(self):
+    QWidget.__init__(self)
+    self.ui = loadUi('uis/select_groups.ui', self)
+    #self.uis.btnTest.clicked.connect(self.__on_test_btn_clicked)
+
+  def __on_test_btn_clicked(self):
+    self.ui.lblMessage.setText('Hello There!' + '\n'+ QDateTime.currentDateTime().toString())
