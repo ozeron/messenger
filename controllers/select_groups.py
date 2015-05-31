2bc6cba5f94e6b2f1dcd701f46ec04219c03a714
Oleksandr Lapchenko <ozeron@me.com>
2015-05-31 14:42:35 +0300

    Improved select groups ui

--- controllers/select_groups.py
+++ controllers/select_groups.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QMessageBox,QAbstractItemView
 from PyQt5.QtCore import QDateTime
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QTableWidget
@@ -13,9 +13,45 @@ class SelectGroupsDialog(QWidget):
     self.vk_client = vk_client
     self.ui = loadUi('uis/select_groups.ui', self)
     self.ui.btnSearch.clicked.connect(self.__on_search_btn_clicked)
+    self.ui.btnAdd.clicked.connect(self.__on_add_btn_clicked)
+    self.ui.btnRemove.clicked.connect(self.__on_delete_btn_clicked)
     self.grid = self.ui.vwvGroups
-    self.grid.itemDoubleClicked.connect(self._double_click_on_group)
     self.grid.setEditTriggers( QTableWidget.NoEditTriggers )
+
+  def __on_add_btn_clicked(self):
+    rows = self.ui.grid.selectionModel().selectedRows()
+    if len(rows) > 0:
+        self.__add_rows(rows)
+    else:
+        self._display_error("Select groups to add!")
+
+  def __add_rows(self, list):
+    self.logger.debug("Trying to add %s" % str(list))
+    for i in range(len(list)):
+        item = list.pop()
+        row = item.row()
+        name = self.grid.item(row, 0).text()
+        id = self.grid.item(row, 1).text()
+        groups_manager.add_favourite_group(id,name)
+        self._highlite_row(item.row(), self._highlite_brush())
+
+  def __delete_rows(self, list):
+    self.logger.debug("Trying to delete %s" % str(list))
+    for i in range(len(list)):
+        item = list.pop()
+        row = item.row()
+        name = self.grid.item(row, 0).text()
+        id = self.grid.item(row, 1).text()
+        groups_manager.remove_from_favourites(id,name)
+        self._highlite_row(item.row(), self._white_brush())
+
+  def __on_delete_btn_clicked(self):
+    rows = self.ui.grid.selectionModel().selectedRows()
+    if len(rows) > 0:
+      self.__delete_rows(rows)
+    else:
+        self._display_error("Select groups to delete!")
+
 
   def __on_search_btn_clicked(self):
     city = self.ui.editCity.text()
@@ -23,7 +59,7 @@ class SelectGroupsDialog(QWidget):
     quantity = int(self.ui.editQuanity.text())
     groups = self.vk_client.vk_messenger.get_top_n_groups_by_location(city, quantity, search_query)
     self.logger.debug('Searching groups top %s in %s with search query %s ', quantity, city, search_query)
-
+    self.grid.setSelectionBehavior(QAbstractItemView.SelectRows);
     self.grid.setColumnCount(2)
     self.grid.setHorizontalHeaderLabels(['Name', 'ID'])
     self.grid.horizontalHeader().setStretchLastSection(True)
@@ -77,3 +113,7 @@ class SelectGroupsDialog(QWidget):
 
   def _white_brush(self):
       return QColor(255,255,255)
+
+  def _display_error(self, msg):
+    self.logger.debug(msg)
+    QMessageBox(QMessageBox.Warning, "Error!", msg).exec()
